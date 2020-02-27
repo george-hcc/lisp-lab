b944c66688a6332b0e2449fd82b62d14d1261be3
George Camboim <george.camboim@embedded.ufcg.edu.br>
2020-02-27 01:58:37 -0300

    Solution to 2.23! Implementation of for-each

---
+++ sicp/chapter2/23.scm
@@ -0,0 +1,8 @@
+#lang sicp
+
+(define (for-each proc list)
+  (cond ((not (null? list))
+         (proc (car list))
+         (for-each proc (cdr list)))))
+
+(for-each (lambda (x) (newline) (display x)) (list 57 321 88))
